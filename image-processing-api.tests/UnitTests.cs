deaaf20f3c3574201d32d7e7c0bcb97ce96db992
georgesavill <gpsavill+github@gmail.com>
2020-09-08 19:32:46 +0100

    Add input test placeholders

---
+++ image-processing-api.tests/UnitTests.cs
@@ -0,0 +1,31 @@
+using System;
+using Xunit;
+
+namespace image_processing_api.tests
+{
+    public class InputTests
+    {
+        [Fact]
+        public void AcceptsInput()
+        {
+
+        }
+        [Fact]
+        public void IsImage()
+        {
+
+        }
+
+        [Fact]
+        public void IsNotImage()
+        {
+
+        }
+
+        [Fact]
+        public void IsNotNull()
+        {
+
+        }
+    }
+}
