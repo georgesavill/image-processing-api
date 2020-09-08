200725785133c9e5663decdcb675c2ecdada3f8f
georgesavill <gpsavill+github@gmail.com>
2020-09-08 18:55:10 +0100

    Add .NET core API and xUnit project templates

---
+++ image-processing-api.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace image_processing_api.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
