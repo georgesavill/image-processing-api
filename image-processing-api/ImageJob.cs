50c2c485c18a6ec1c8b92874d84b980f2abad39a
georgesavill <gpsavill+github@gmail.com>
2020-09-08 20:31:23 +0100

    Add Image job classes

---
+++ image-processing-api/ImageJob.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_processing_api
+{
+    public class ImageJob
+    {
+        public byte[] ImageBytes { get; set; }
+        public ImageJobOptions Options { get; set; }
+    }
+}
---
+++ image-processing-api/ImageJobOptions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace image_processing_api
+{
+    public class ImageJobOptions
+    {
+        public string Format { get; set; }
+        public string Size { get; set; }
+        public string Quality { get; set; }
+    }
+}
