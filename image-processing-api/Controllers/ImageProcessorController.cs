9127304f1bcd8a5205076cd93a754013b26707f6
georgesavill <gpsavill+github@gmail.com>
2020-09-08 19:32:32 +0100

    Rename and remove template content

---
+++ image-processing-api/Controllers/ImageProcessorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace image_processing_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageProcessorController : ControllerBase
+    {
+        private readonly ILogger<ImageProcessorController> _logger;
+
+        public ImageProcessorController(ILogger<ImageProcessorController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "hello world";
+        }
+    }
+}
